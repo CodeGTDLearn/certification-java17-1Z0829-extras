58040de453da5d0aeb0113de90c4ee6ec2c1aa01
PauloPortofolio <pvba02@gmail.com>
2023-07-19 15:40:00 -0400

    17) Java Extras
    -> CONCLUDED:
    - TextBlock - IMPROVEMENTS v2
    
    -> WORKING:
    
    -> NEXT STEPS:
    
    -> FUTURE IMPLEMENTATIONS:

--- src/main/java/extras/textblocks/SpecialChars_lenght.java
+++ src/main/java/extras/textblocks/SpecialChars_lenght.java
@@ -1,6 +1,6 @@
 package extras.textblocks;
 
-import static java.lang.String.*;
+import static java.lang.String.valueOf;
 import static java.lang.System.out;
 
 public class SpecialChars_lenght {
@@ -10,49 +10,46 @@ public class SpecialChars_lenght {
          1   \
          2""";
     sout("joiner_5", valueOf(joiner_5.length()));
-    out.println(joiner_5);
-//-----------------------------------------------------------------------------
+    out.println(joiner_5 + "\n---------------------------------");
 
     String next_trailing_8 = """
          1    \n
          2""";
     sout("next_trailing_8", valueOf(next_trailing_8.length()));
-    out.println(next_trailing_8);
+    out.println(next_trailing_8 + "\n---------------------------------");
 
     String next_implic_4 = """
          1      
          2
          """;
     sout("next_implic_4", valueOf(next_implic_4.length()));
-    out.println(next_implic_4);
-//-----------------------------------------------------------------------------
+    out.println(next_implic_4 + "\n---------------------------------");
 
     // '/n' is implicity resulting in 6
     String trailing_singleSpace_6 = """
          1\s\s\s
          2""";
     sout("trailing_singleSpace_6", valueOf(trailing_singleSpace_6.length()));
-    out.println(trailing_singleSpace_6);
+    out.println(trailing_singleSpace_6 + "\n---------------------------------");
 
     // '/n' is implicity resulting in 10
     String trailing_singleSpace_10 = """
          1      \s
          2""";
     sout("trailing_singleSpace_10", valueOf(trailing_singleSpace_10.length()));
-    out.println(trailing_singleSpace_10);
-//-----------------------------------------------------------------------------
+    out.println(trailing_singleSpace_10 + "\n---------------------------------");
 
     String tab_4 = """
          1\t
          2""";
     sout("tab_4", valueOf(tab_4.length()));
-    out.println(tab_4);
+    out.println(tab_4 + "\n---------------------------------");
 
     String trailing_tab_single_7 = """
          1  \t\t
          2""";
     sout("trailing_tab_single_7", valueOf(trailing_tab_single_7.length()));
-    out.println(trailing_tab_single_7);
+    out.println(trailing_tab_single_7 + "\n---------------------------------");
   }
 
   private static void sout(String... text) {
